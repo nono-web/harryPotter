20c7641221c73d04c4d9bf40134c38852f5bfcf2
[REM-FR]Arnaud Didier <84334523+nono-web@users.noreply.github.com>
2023-01-03 15:07:44 +0100

    router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import AllPersoList from './Pages/AllPersoList';
+import Contact from "./Pages/Contact";
+import Home from "./Pages/Home";
 
 
 const App = () => {
   return (
     <div>
-   Harry Potter
+<Router>
+  <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/allperso" element={<AllPersoList/>}/>
+    <Route path="/contact" element={<Contact/>}/>
+  </Routes>
+</Router>
+
     </div>
   );
 }
